a4d3398788286193fe0bd14df32107946f74d5dd
John Muthua <caveomuthama@gmail.com>
2023-04-20 14:10:55 +0300

    Cmplete functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 function fillOut() {
   var fill = document.getElementById("bars");
   var cover = document.getElementById("cover");
-  console.log(cover);
   if (fill.className === "burger") {
     fill.className += " burger-icon-hide filler";
     cover.style.display = "block";
   } else {
     fill.className = "burger";
   }
+}
+
+function closeCover() {
+  var cover = document.getElementById("cover");
+  var fill = document.getElementById("bars");
+  cover.style.display = "none";
+  fill.className = "burger";
 }
