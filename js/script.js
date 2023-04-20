0d4b099b99b13c17f5256f81b0e8b0911d35ea45
John Muthua <caveomuthama@gmail.com>
2023-04-20 13:34:56 +0300

    Add cover

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function fillOut() {
   var fill = document.getElementById("bars");
+  var cover = document.getElementById("cover");
+  console.log(cover);
   if (fill.className === "burger") {
-    fill.className += " filler";
+    fill.className += " burger-icon-hide filler";
+    cover.style.display = "block";
   } else {
     fill.className = "burger";
   }
-  console.log("clicked object");
 }
